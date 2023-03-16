0ba62259899ce9fe08f59d1e15fad6d57e3b3cf8
Narveer Rathore <rathore.narveer1994@gmail.com>
2023-03-16 21:33:44 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 app.get("/", (req, res) => {
   res.set('Set-Cookie', "embeddedCookie=Hello from an embedded third party cookie!;Path=/;Secure;SameSite=None");
-  res.redirect(req.headers.referer + "?key=test");
+  res.redirect(req.headers.referer + "/static?key=test");
 });
 app.listen(5000, () => {
   console.log("Running on port 5000.");
